6dde76005c316081e87fa29f636951285cd57a47
liu <461043365@qq.com>
2019-04-24 10:36:05 +0000

    获取用户信息

--- app/Http/Controllers/Weixin/TestController.php
+++ app/Http/Controllers/Weixin/TestController.php
@@ -82,7 +82,9 @@ class TestController extends Controller
     public function wxweb(){
         $code=$_GET['code'];
         $url1='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
-        $access_token=json_decode(file_get_contents($url1),true);
-        print_r($access_token);
+        $arr=json_decode(file_get_contents($url1),true);
+        $url2='https://api.weixin.qq.com/sns/userinfo?access_token='.$arr['access_token'].'&openid='.$arr['openid'].'&lang=zh_CN';
+        $userInfo=json_decode(file_get_contents($url2),true);
+        echo '<pre>' .print_r($userInfo);echo '<hr>';
     }
 }
