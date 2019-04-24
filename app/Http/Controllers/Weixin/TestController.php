72eaaf6bc2fceaa77a8084cee2e8a08347b0e118
liu <461043365@qq.com>
2019-04-24 11:03:05 +0000

    a

--- app/Http/Controllers/Weixin/TestController.php
+++ app/Http/Controllers/Weixin/TestController.php
@@ -84,8 +84,6 @@ class TestController extends Controller
         $code=$_GET['code'];
         $url1='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $arr=json_decode(file_get_contents($url1),true);
-        print_r(<pre>);
-        print_r($arr);die;
         $url2='https://api.weixin.qq.com/sns/userinfo?access_token='.$arr['access_token'].'&openid='.$arr['openid'].'&lang=zh_CN';
         $userInfo=json_decode(file_get_contents($url2),true);
         $user_info=[
