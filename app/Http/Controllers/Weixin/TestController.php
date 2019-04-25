c82dc9079f995e056ed117accfe2bc8580d38948
liu <461043365@qq.com>
2019-04-25 11:46:28 +0000

    获取临时素材

--- app/Http/Controllers/Weixin/TestController.php
+++ app/Http/Controllers/Weixin/TestController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use App\Model\User;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
 class TestController extends Controller
 {
     //微信推送
@@ -49,9 +51,27 @@ class TestController extends Controller
                           </Articles>
                         </xml>';
                 }
-
-
             }
+
+
+        }else if($data->MsgType=='image'){
+            $access_token=getWxAccessToken();
+            //请求地址
+            $url='https://api.weixin.qq.com/cgi-bin/media/get?access_token='.$access_token.'&media_id='.$data->MediaId;
+            //接口数据
+            $clinet=new Client();
+            $response=$clinet->request('GET',$url);
+
+            //获取文件名称
+            $file_info=$response->getHeader('Content-disposition');
+            $file_name = substr(md5(time().mt_rand(10000,99999)),10,8);
+            $file_newname = $file_name.'_'.substr(rtrim($file_info[0],'"'),-20);
+
+            //保存图片
+            $image=$response->getBody();
+            $wx_image_path = 'wx_media/images/'.$file_newname;
+            $rr = Storage::disk('local')->put($wx_image_path,$image);
+            var_dump($rr);
         }
     }
     //商品详情
